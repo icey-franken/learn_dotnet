fedcc207a93b34ca06051ca735ae7eea14fb96d3
Isaac Finken <isaac.finken@intimetec.com>
2021-05-12 12:49:32 -0600

    C# fundamentals - start of module 3.

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,6 +6,9 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            double x = 17;
+            double y = 18;
+            Console.WriteLine(x + y);
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
