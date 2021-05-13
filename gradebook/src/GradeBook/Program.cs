0728a3411d75df24541262273621745ed1a725fb
Isaac Finken <isaac.finken@intimetec.com>
2021-05-13 15:32:18 -0600

    Add Book class.

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,14 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Program
     {
+        class Book 
+        {
+            
+        }
+
         static void Main(string[] args)
         {
-            double x = 17;
-            double y = 18;
-            Console.WriteLine(x + y);
+            var book = new Book()
+
+            var grades = new List<double>() { 5, 7, 9, 5 };
+            grades.Add(9);
+            
+            double total = 0;
+            foreach (double number in grades)
+            {
+                total += number;
+            }
+            var average = total / grades.Count;
+            Console.WriteLine($"The average grade is {average:N2}.");
+            
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
