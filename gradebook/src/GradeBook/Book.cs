fb0a477357b43713bde175fac9f2d3efbdb87c9a
Isaac Finken <isaac.finken@intimetec.com>
2021-05-14 10:17:20 -0600

    Complete C# Fundamentals - Module 4 - Working with Classes and Objects.

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,7 +1,51 @@
+using System;
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     class Book
     {
+        // define a constructor - no return type (void, etc)
+        public Book()
+        {
+            grades = new List<double>();
+        }
+        // define a method
+        public void AddGrade(double grade)
+        {
+            // validate grade
+
+            // store in an object instantiated by this class  as a piece of state
+            grades.Add(grade);
+        }
+
+        public void AddGrades(double[] grades)
+        {
+            foreach (double grade in grades)
+            {
+                this.AddGrade(grade);
+            }
+        }
+
+        public void ShowStats()
+        {
+
+            var total = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (double number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                total += number;
+            }
+            var average = total / grades.Count;
+            Console.WriteLine($"The average grade is {average:N2}.\n The highest grade is {highGrade:N1}.\n The lowest grade is {lowGrade:N1}.");
+        }
+
+        // define a field - CANNOT use implicit typing (var)
+        private List<double> grades;
+        private string name;
 
     }
 }
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -5,34 +5,16 @@ namespace GradeBook
 {
     class Program
     {
-        class Book 
-        {
-            
-        }
 
         static void Main(string[] args)
         {
-            var book = new Book()
-
-            var grades = new List<double>() { 5, 7, 9, 5 };
-            grades.Add(9);
-            
-            double total = 0;
-            foreach (double number in grades)
-            {
-                total += number;
-            }
-            var average = total / grades.Count;
-            Console.WriteLine($"The average grade is {average:N2}.");
-            
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello, stranger!");
-            }
+            var book = new Book();
+            book.AddGrade(89.1);
+            book.AddGrade(0.69);
+            book.AddGrade(420);
+            double[] grades = { 10.0, 20.0, 30.0 };
+            book.AddGrades(grades);
+            book.ShowStats();
         }
     }
 }
