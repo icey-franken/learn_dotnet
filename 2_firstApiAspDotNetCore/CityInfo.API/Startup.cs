ab2c8ec3a244f852913782cdace47cbba9575135
Isaac Finken <isaac.finken@intimetec.com>
2021-05-19 11:07:54 -0600

    Renaming and moving things around - messy.

--- 2_firstApiAspDotNetCore/CityInfo.API/Startup.cs
+++ 2_firstApiAspDotNetCore/CityInfo.API/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Project
+namespace CityInfo.API
 {
     public class Startup
     {
