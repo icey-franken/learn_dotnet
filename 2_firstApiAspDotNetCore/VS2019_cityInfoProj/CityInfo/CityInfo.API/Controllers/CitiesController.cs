5093cbf6bff413cfc5969c52982dc1d503d1548a
Isaac Finken <isaac.finken@intimetec.com>
2021-05-20 13:20:57 -0600

    First API with ASP.NET Core - working through module 3.

---
+++ 2_firstApiAspDotNetCore/VS2019_cityInfoProj/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    //use route attribute at controller level to add a URI 'template'. Now, all routes with 'api/cities' will
+    //  go through this controller, and actions within do not need 'api/cities' prefixed.
+    [Route("api/cities")]
+    //OR
+    //[Route("api/[controller]")]
+    //we can do this because our controller is named CitiesController. Note that this could create issues when 
+    //  refactoring, because if we change the name of our controller, all requests will be routed to a new place.
+    public class CitiesController : ControllerBase
+    {
+        //adding an HttpGet attribute to be executed when request sent to "api/cities"
+        [HttpGet]
+        //class JsonResult returns jsonified version of whatever passed into constructor of new JsonResult object
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        //remember: we set the route template to api/cities, so we only look for id here to find api/cities/id
+        //  curly braces are used to indicate a parameter
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == id));
+        }
+    }
+}
--- 2_firstApiAspDotNetCore/VS2019_cityInfoProj/CityInfo/CityInfo.API/Startup.cs
+++ 2_firstApiAspDotNetCore/VS2019_cityInfoProj/CityInfo/CityInfo.API/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -14,11 +15,15 @@ namespace CityInfo.API
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //this is where we 'add framework services to the container'
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(MvcOptions => MvcOptions.EnableEndpointRouting = false);
+            //MvcOptions.EnableEndpointRouting = false;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //this is where we add middlewares
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -30,16 +35,29 @@ namespace CityInfo.API
                 app.UseExceptionHandler();
             }
 
-            app.UseRouting();
+            app.UseMvc();
 
-            app.UseEndpoints(endpoints =>
+            app.Run(async (context) =>
             {
-                endpoints.MapGet("/", context =>
-                {
-                    //await context.Response.WriteAsync("Hello Borld!");
-                    throw new Exception("Example exception");
-                });
+                await context.Response.WriteAsync("Hello, Borld!");
             });
+
+            //routing matches request URI to controller METHOD
+            //two types of routing: convention based and attribute based - we will use attribute based.
+            //  convention: need to be configured; typically used if returning HTML views. Not rec for APIs
+            //  attribute: allows use of attributes at controller and action level; can have optional template.
+            //    URI matched to a specific action on a controller
+            //    there are controller level and action level attributes
+            //      action level attributes (HttpGet, HttpPost, etc.) are members of a controller level attribute (route)
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello Borld!");
+            //    });
+            //});
         }
     }
 }
