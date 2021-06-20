3f857334de0c96c5ba962a97092c59d6d2c8a5ad
Isaac Finken <isaac.finken@intimetec.com>
2021-06-20 17:40:34 -0600

    Add validations to entity.

--- 2_firstApiAspDotNetCore/VS2019_cityInfoProj/CityInfo/CityInfo.API/Entities/PointOfInterest.cs
+++ 2_firstApiAspDotNetCore/VS2019_cityInfoProj/CityInfo/CityInfo.API/Entities/PointOfInterest.cs
@@ -8,6 +8,14 @@ namespace CityInfo.API.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+        //EF core automatically sets CityId as a foreign key here (convention based) but we can explicitly set it
+        //to be clear - we explicity create the foreign key CityId and can name it whatever we want by including the ForeignKey decorator
+        //and specifiying the name in parens.
+        [ForeignKey("CityId")]
+        public City City { get; set; }
+        public int CityId { get; set; }
     }
 }
