a978bc095bf326a7767de09b4a35632502e956a6
Isaac Finken <isaac.finken@intimetec.com>
2021-06-20 17:32:12 -0600

    First API with ASP.NET Core - start module 6.

---
+++ 2_firstApiAspDotNetCore/VS2019_cityInfoProj/CityInfo/CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
