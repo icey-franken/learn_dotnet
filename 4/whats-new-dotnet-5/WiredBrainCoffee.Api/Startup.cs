dbac690b9f24659320becccbf591216399ead8e1
Isaac Finken <isaac.finken@intimetec.com>
2021-07-05 22:03:11 -0600

    Whats new with ASP.NET Core in .NET 5.0 - finished Module 2.

--- 4/whats-new-dotnet-5/WiredBrainCoffee.Api/Startup.cs
+++ 4/whats-new-dotnet-5/WiredBrainCoffee.Api/Startup.cs
@@ -28,10 +28,10 @@ namespace WiredBrainCoffee.Api
         {
 
             services.AddControllers();
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WiredBrainCoffee.Api", Version = "v1" });
-            //});
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WiredBrainCoffee.Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
