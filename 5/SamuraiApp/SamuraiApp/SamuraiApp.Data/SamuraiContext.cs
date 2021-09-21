da5a69f31c8b650e9e2306d2029d3ad9a43513c0
Isaac Finken <isaac.finken@intimetec.com>
2021-09-21 12:05:15 -0600

    Completed Module 8 of course 'Entity Framework Core: Getting Started'.

--- 5/SamuraiApp/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ 5/SamuraiApp/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -1,32 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
-using System;
 
 namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+
+        }
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
         public DbSet<Horse> Horses { get; set; }
         public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder
-                .UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData")
-                .LogTo(
-                    Console.WriteLine, 
-                    new[] { 
-                        DbLoggerCategory.Database.Command.Name,
-                        DbLoggerCategory.Database.Transaction.Name
-                    }, 
-                    LogLevel.Information
-                )
-                .EnableSensitiveDataLogging();
-        }
+        //this is now in startup.cs in our SamuraiAPI
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder
+        //        .UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData")
+        //        .LogTo(
+        //            Console.WriteLine,
+        //            new[] {
+        //                DbLoggerCategory.Database.Command.Name,
+        //                DbLoggerCategory.Database.Transaction.Name
+        //            },
+        //            LogLevel.Information
+        //        )
+        //        .EnableSensitiveDataLogging();
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
